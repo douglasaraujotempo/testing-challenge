80ac0f21aab8de83a810fd1704c8fffcc5a86176
Douglas Araujo <douglas.araujo@tempo.io>
2023-01-06 14:26:48 -0300

    testing

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -3,6 +3,12 @@ import { Container } from './styles';
 
 export default function BackButton({ history }) {
 
+
+
+
+
+
+    
     return (
         <Container onClick={() => { history.goBack() }}>Return</Container>
     )
