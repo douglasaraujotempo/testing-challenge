4f9ab63a374643cb3476e5d90d2b9375854f2ede
Douglas Araujo <douglas.araujo@tempo.io>
2023-01-06 14:27:21 -0300

    duplicate

---
+++ src/components/BackButton/indexv2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from './styles';
+
+export default function BackButton({ history }) {
+
+
+
+
+
+
+    
+    return (
+        <Container onClick={() => { history.goBack() }}>Return</Container>
+    )
+}
